b7142869f2f9956b2e098f6dbeedb18b538f0bf9
Voufack Harold <voufackharold@gmail.com>
2022-04-14 13:57:59 +0100

    Added Rest controller

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class Controller {
+	
+	@GetMapping()
+	@ResponseBody
+	public String test() {
+		return "Hello World !!!";
+	}
+
+}
